3f50f251f3d398ee7ff6f38b5680251e9723c113
Harviator <78506767+harviator@users.noreply.github.com>
2021-06-02 13:31:31 -0400

    made styling changes to landing page that effected html, css, and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,15 @@
-// Date Picker //
-var autoupdate = false;
+//JQuery Date Picker
+//Date picker 1
+$( function() {
+    $( "#datepicker" ).datepicker();
+ } 
+);
 
-function date1(){
-$('.date-picker').daterangepicker({
-  singleDatePicker: true,
-  showDropdowns: true,
-  minDate: 'today',
-  autoApply: true,
-  autoUpdateInput: autoupdate,
-  locale: {
-    format: 'YYYY-MM-DD'
-  }
-}, function(chosen_date) {
-  $('.date-picker').val(chosen_date.format('YYYY-MM-DD'));
-});
-  };
-date1();
-
-$('.date-picker-2').daterangepicker({
-  singleDatePicker: true,
-  showDropdowns: true,
-  minDate: 'today',
-  autoApply: true,
-  autoUpdateInput: false,
-  locale: {
-    format: 'YYYY-MM-DD'
-  }
-}, function(chosen_date) {
-  $('.date-picker-2').val(chosen_date.format('YYYY-MM-DD'));
-});
-
-$('.date-picker').on('apply.daterangepicker', function(ev, picker) {
-    if ($('.date-picker').val().length == 0 ){
-    autoupdate = true;
-    console.log('true');
-    date1();
-  };
-  var departpicker = $('.date-picker').val();
-  $('.date-picker-2').daterangepicker({
-    minDate: departpicker,
-    singleDatePicker: true,
-    showDropdowns: true,
-    autoApply: true,
-    locale: {
-      format: 'YYYY-MM-DD'
-    }
-  });
-  
-  var drp = $('.date-picker-2').data('daterangepicker');
-  drp.setStartDate(departpicker);
-  drp.setEndDate(departpicker);
-});
-
-
-$('#clear').click(function(){
- $('input.form-control').val('');
-});
+//Date picker 2
+$( function() {
+    $( "#datepicker2" ).datepicker();
+ } 
+);
 
 
 // // ELISONS PORTION
