9347f424a6c147ce7ef5e0bc552de6a010b42d0a
Elison Liu <elisonliu97@gmail.com>
2021-06-02 13:54:08 -0400

    fixed some functionalities

--- assets/js/script.js
+++ assets/js/script.js
@@ -206,20 +206,6 @@ function fetchHotelImage(hId){
     })
 }
 
-//  NOTE: TO CREATE MARKERS
-function createMarker(place) {
-  if (!place.geometry || !place.geometry.location) return;
-  const marker = new google.maps.Marker({
-    map,
-    position: place.geometry.location,
-  });
-  google.maps.event.addListener(marker, "click", () => {
-    infowindow.setContent(place.name || "");
-    infowindow.open(map);
-  });
-}
-
-
 // CREATE CARDS FOR RESULTS
 
 buttonEl.addEventListener('click', formSubmitHandler);
