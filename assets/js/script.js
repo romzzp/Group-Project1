f3ea5d7b22d4fbce678ed3fc7595168891b8ed9d
romzz.git <romzz.patel@gmail.com>
2021-06-03 20:07:31 +0530

    fixed cards issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 //JQuery Date Picker
 //Date picker 1
-$( function() {
-    $( "#datepicker" ).datepicker();
- } 
+$(function () {
+    $("#datepicker").datepicker();
+}
 );
 
 //Date picker 2
-$( function() {
-    $( "#datepicker2" ).datepicker();
- } 
+$(function () {
+    $("#datepicker2").datepicker();
+}
 );
 
 
@@ -35,7 +35,7 @@ function formSubmitHandler(event) {
     // populate datein
     // populate dateout
     // populate roomnum
-    
+
     geocodeApiFunc(locInput);
 
 
@@ -45,92 +45,92 @@ function formSubmitHandler(event) {
 // // FETCH APIS
 
 // function to translate city names to geocode
-function geocodeApiFunc (loc) {
-    var geocodeUrl = "https://geocode.search.hereapi.com/v1/geocode?q="+locInput+"&apiKey=D1Hv_zXz0JAD-U38n3BNT1zWKGjjPAFX9gsbKb2EwFw";
+function geocodeApiFunc(loc) {
+    var geocodeUrl = "https://geocode.search.hereapi.com/v1/geocode?q=" + locInput + "&apiKey=D1Hv_zXz0JAD-U38n3BNT1zWKGjjPAFX9gsbKb2EwFw";
     fetch(geocodeUrl)
-        .then(function (response){
-            response.json().then(function (data){
+        .then(function (response) {
+            response.json().then(function (data) {
                 var latVal = data.items[0].position.lat;
                 var lngVal = data.items[0].position.lng;
-                console.log(lngVal,latVal);
+                console.log(lngVal, latVal);
                 hotelApiFunc(latVal, lngVal, checkInDate, checkOutDate, roomNum);
-            })        
-            .catch(function(error){
-                console.log(error);
             })
+                .catch(function (error) {
+                    console.log(error);
+                })
         })
-    
+
 }
 
 // function to get results from hotel api
-function hotelApiFunc (lat, lon, checkin, checkout, rooms){
-    fetch("https://hotels-com-free.p.rapidapi.com/srle/listing/v1/brands/hotels.com?lat="+lat+"&lon="+lon+"&checkIn="+checkin+"&checkOut="+checkout+"&rooms="+rooms+"&locale=en_US&currency=CAD&pageNumber=1", {
+function hotelApiFunc(lat, lon, checkin, checkout, rooms) {
+    fetch("https://hotels-com-free.p.rapidapi.com/srle/listing/v1/brands/hotels.com?lat=" + lat + "&lon=" + lon + "&checkIn=" + checkin + "&checkOut=" + checkout + "&rooms=" + rooms + "&locale=en_US&currency=CAD&pageNumber=1", {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "475606eb04msh3305527aef63460p1ea2ccjsn5d20c76f0ac9",
             "x-rapidapi-host": "hotels-com-free.p.rapidapi.com"
         }
     })
-    .then(function (response) {
-        response.json().then(function (responsejson){
-            var hotelList = responsejson.data.body.searchResults.results;
-            console.log(hotelList);
-            for (var i = 0; i < hotelList.length; i++){
-                createHotelCard(hotelList[i])
-            }
-            // USING THIS LIST, POPULATE WITH A CARD MAKER
+        .then(function (response) {
+            response.json().then(function (responsejson) {
+                var hotelList = responsejson.data.body.searchResults.results;
+                console.log(hotelList);
+                for (var i = 0; i < hotelList.length; i++) {
+                    createHotelCard(hotelList[i])
+                }
+                // USING THIS LIST, POPULATE WITH A CARD MAKER
+            })
         })
-    })
-    .catch(function(error){
-        console.error(err);
-    });
+        .catch(function (error) {
+            console.error(err);
+        });
 }
 
 //Variable to test hotel cards
-var dummyHotel = {
-    address: {
-        streetAddress: '123 Somewhere St.'
-    },
-    name: 'Dummy Hotel',
-    ratePlan: {
-        price: {
-            current: "$1,000"
-        }
-    },
-    guestReviews: {
-        rating: "1 Star"
-    }
-}
+// var dummyHotel = {
+//     address: {
+//         streetAddress: '123 Somewhere St.'
+//     },
+//     name: 'Dummy Hotel',
+//     ratePlan: {
+//         price: {
+//             current: "$1,000"
+//         }
+//     },
+//     guestReviews: {
+//         rating: "1 Star"
+//     }
+// }
 // function to create card for results gotten back from hotel api
 // need to stick hotelid somewhere
-function createHotelCard(hotel){
+function createHotelCard(hotel) {
     // var hotelId = hotel.id;
     // var hLat = hotel.coordinate.lat
     // var hLon = hotel.coordinate.lon
-    var address = hotel.address.streetAddress ;
-    if (!address){
+    var address = hotel.address.streetAddress;
+    if (!address) {
         address = "NO ADDRESS"
     }
     var name = hotel.name;
-    if (!name){
+    if (!name) {
         name = "NO NAME";
     }
     var ratings = hotel.guestReviews
-    if (!ratings){
+    if (!ratings) {
         ratings = "NO REVIEWS";
     } else {
         ratings = hotel.guestReviews.rating
-        if (!ratings){
+        if (!ratings) {
             ratings = "NO RATINGS";
         }
     }
     var price = hotel.ratePlan
-    if (!price){
+    if (!price) {
         price = "NO PRICE";
     } else {
         price = hotel.ratePlan.price.current
-        if (!price){
-            price="NO PRICE";
+        if (!price) {
+            price = "NO PRICE";
         }
     }
 
@@ -149,7 +149,7 @@ function createHotelCard(hotel){
     var cardFooter = document.createElement('div');
     cardFooter.setAttribute('class', 'card-footer');
 
-    document.getElementsByClassName('mainDiv')[0].append(cardDiv);
+    document.getElementsByClassName('cardContainer')[0].append(cardDiv);
     cardDiv.append(cardHeader);
     cardHeader.insertAdjacentElement('afterend', cardContent);
     cardContent.insertAdjacentElement('afterend', cardFooter);
@@ -171,30 +171,30 @@ function createHotelCard(hotel){
     // hCardEl.appendChild(hParaEl);
 }
 
-createHotelCard(dummyHotel);
+// createHotelCard(dummyHotel);
 
 // HOTEL IMAGE API
 // returns an url for an image of the room
 // NOTE: TRY PUTTING IN A TIMER TO DELAY BY A FEW SECONDS
 // NOTE: TRY USING the data-hotel-id attribute to get the hId
-function fetchHotelImage(hId){
-    fetch("https://hotels-com-free.p.rapidapi.com/nice/image-catalog/v2/hotels/"+hId, {
-    	"method": "GET",
-	    "headers": {
-		    "x-rapidapi-key": "475606eb04msh3305527aef63460p1ea2ccjsn5d20c76f0ac9",
-		    "x-rapidapi-host": "hotels-com-free.p.rapidapi.com"
-	    }
+function fetchHotelImage(hId) {
+    fetch("https://hotels-com-free.p.rapidapi.com/nice/image-catalog/v2/hotels/" + hId, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "475606eb04msh3305527aef63460p1ea2ccjsn5d20c76f0ac9",
+            "x-rapidapi-host": "hotels-com-free.p.rapidapi.com"
+        }
     })
-    .then(function (response) {
-        response.json().then(function (responsejson){
-            var imgsrc = responsejson.hotelImages[0].baseUrl;
-            newImgsrc = imgsrc.replace("_{size}", "")
-            return (newImgsrc)
+        .then(function (response) {
+            response.json().then(function (responsejson) {
+                var imgsrc = responsejson.hotelImages[0].baseUrl;
+                newImgsrc = imgsrc.replace("_{size}", "")
+                return (newImgsrc)
+            })
         })
-    })
-    .catch(function(error){
-	    console.log(error);
-    })
+        .catch(function (error) {
+            console.log(error);
+        })
 }
 
 // CREATE CARDS FOR RESULTS
